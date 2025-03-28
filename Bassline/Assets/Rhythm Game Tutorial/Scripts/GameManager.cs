3db7211b7c3fdf1e912e591c8c6bbb1707505ec4
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2025-03-28 14:51:00 -0700

    Tutorial Part #3 FINISHED
    https://youtu.be/dV9rdTlMHxs?list=PLLPYMaP0tgFKZj5VG82316B63eet0Pvsv
    thank you gamesplusjames

--- Bassline/Assets/Rhythm Game Tutorial/Scripts/GameManager.cs
+++ Bassline/Assets/Rhythm Game Tutorial/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,10 +10,22 @@ public class GameManager : MonoBehaviour
     public BeatScroller theBS;
     public static GameManager instance;
 
+    public int currentScore;
+    public int scorePerNote = 100;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;        
+        instance = this;
+
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -34,9 +47,30 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("Note hit :3");
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
     }
+
     public void NoteMissed()
     {
         Debug.Log("note MISSED </3");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+        multiText.text = "Multiplier: x" + currentMultiplier;
     }
 }
